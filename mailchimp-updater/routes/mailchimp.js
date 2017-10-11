cdd14afc387dfaa096f7b34488acf7775c2214dc
Joel Myers <lokaideveloper@gmail.com>
2017-10-11 10:38:06 -0400

    adding route for mailchimp and sketching out script in comments

---
+++ mailchimp-updater/routes/mailchimp.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  // OPEN CSV
+
+  // FOREACH EMAIL
+
+    // CONVERT TO LOWER CASE
+
+    // COMPUTE MD5 HASH
+
+    // SUBMIT PATCH REQUEST
+    // e.g., https://us6.api.mailchimp.com/3.0/lists/bada47bbab/members/6df921a0eeb3266d9cb16f276c655e66
+
+    // output response to make sure it went through ok
+});
+
+module.exports = router;
